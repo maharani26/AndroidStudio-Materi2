ff652fdba519f012d8dee22f2b92c45aaff29122
Maharani Annisa Pasha <maharani_annisa_26rpl@student.smktelkom-mlg.sch.id>
2019-04-01 14:06:39 +0700

    selesai

---
+++ app/src/main/java/id/sch/smktelkommlg/www/relativelayout_rani/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkommlg.www.relativelayout_rani;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
